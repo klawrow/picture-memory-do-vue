498a5983891b573c47fb18b2aeaa810a45a11bc3
Claro A. Briones <6843695+klawrow@users.noreply.github.com>
2021-01-10 13:19:49 -0600

    Moves github and twitter links to left of header

--- src/components/header-bar-brand.vue
+++ src/components/header-bar-brand.vue
@@ -3,5 +3,26 @@
     <div class="navbar-item nav-home">
       <router-link class="start-over" to="/">START OVER</router-link>
     </div>
+    <div class="navbar-item">
+      <div class="buttons">
+        <a :href="github" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-github" aria-hidden="true"></i>
+        </a>
+        <a :href="twitter" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-twitter" aria-hidden="true"></i>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      github: "https://github.com/klawrow/picture-memory-do-vue",
+      twitter: "https://twitter.com/klawrow",
+    };
+  },
+};
+</script>
--- src/components/header-bar-links.vue
+++ src/components/header-bar-links.vue
@@ -3,14 +3,8 @@
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a :href="github" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
-          </a>
-          <a :href="twitter" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
-          </a>
           <a :href="vuejs" target="_blank" rel="noopener noreferrer">
-            <i class="fab js-logo fa-vuejs fa-2x" aria-hidden="true"></i>
+            <i class="fab js-logo fa-vuejs" aria-hidden="true"></i>
           </a>
         </div>
       </div>
@@ -22,8 +16,6 @@
 export default {
   data() {
     return {
-      github: "https://github.com/klawrow/picture-memory-do-vue",
-      twitter: "https://twitter.com/klawrow",
       vuejs: "https://vuejs.org/",
     };
   },
